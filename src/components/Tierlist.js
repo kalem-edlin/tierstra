051ac0d6b9321dc03cdac58acc28d15f164845d2
Kalem Edlin <kalem.edlin@trademe.co.nz>
2022-07-02 03:20:21 +1200

    MVP-001: Used a type prop on the extra draggable and dropable contexts in order to separate the row dragging from the tile dragging.

--- src/components/Tierlist.js
+++ src/components/Tierlist.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Typography, Box, Container } from '@mui/material';
+import { Typography, Box, Container, useTheme } from '@mui/material';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import initialData from '../data/InitialData'
 
 export default function Tierlist() {
     const [data, setData] = useState(initialData);
+    const theme = useTheme();
 
     const onDragEnd = result => {
         //apply changes to data arrays/persist draggable components
@@ -13,12 +14,12 @@ export default function Tierlist() {
 
     return (
         <DragDropContext onDragEnd={onDragEnd}>
-            <Container maxWidth="lg" sx={{ mt: 8 }} >
-                {/* <Droppable droppableId="tierlist" direction="vertical">
-                    {provided => ( */}
+            <Container maxWidth="lg" sx={{ mt: 8 }}>
+                <Droppable droppableId="tierlist" direction="vertical" type={"row"}> 
+                    {provided => (
                         <Box
-                            // ref={provided.innerRef} 
-                            // {...provided.droppableProps} 
+                            ref={provided.innerRef} 
+                            {...provided.droppableProps} 
                         >
                             {data.rowOrder.map((rowId, index) => {
                                 const row = data.rows[rowId];
@@ -26,8 +27,8 @@ export default function Tierlist() {
                                 return <Row key={row.id} row={row} tiles={tiles} index={index}/>;
                             })}
                         </Box>
-                    {/* )}
-                </Droppable> */}
+                    )}
+                </Droppable>
             </Container>
         </DragDropContext>
     )
@@ -36,29 +37,29 @@ export default function Tierlist() {
 
 function Row(props) {
     return (
-        // <Draggable draggableId={"row"+props.row.id} index={props.index}>
-        //     {provided => (
+        <Draggable draggableId={"row"+props.row.id} index={props.index} type={"row"}>
+            {provided => (
                 <Box 
-                    // ref={provided.innerRef} 
-                    // {...provided.draggableProps} 
+                    ref={provided.innerRef} 
+                    {...provided.draggableProps} 
                     sx={{ mb: 2, border: '1px solid lightgrey', borderRadius: '2px', display: 'flex', flexDirection: 'row', width: 1, height: 100 }}
                 >
                     <Box 
-                        // {...provided.dragHandleProps} 
+                        {...provided.dragHandleProps} 
                         sx={{ border: '1px solid black', borderRadius: '2px', width: 0.15, height: 1 }}
                     >
                         <TextBox variant="h5"> {props.row.title} </TextBox>
                     </Box>
                     <TileList key={props.row.id} row={props.row} tiles={props.tiles}/>
                 </Box>
-        //     )}
-        // </Draggable>
+            )}
+        </Draggable>
     )
 }
 
 function TileList(props) {
     return (
-        <Droppable droppableId={"tile"+props.row.id} direction="horizontal">
+        <Droppable droppableId={"tile"+props.row.id} direction="horizontal" type={"tile"}>
             {provided => (
                 <Box 
                     ref={provided.innerRef} 
@@ -77,7 +78,7 @@ function TileList(props) {
 
 function Tile(props) {
     return (
-        <Draggable draggableId={props.tile.id} index={props.index}>
+        <Draggable draggableId={props.tile.id} index={props.index} type={"tile"}>
             {provided => (
                 <Box
                     ref={provided.innerRef} 
@@ -94,7 +95,7 @@ function Tile(props) {
 
 function TextBox(props) {
     return (
-        <Box sx={{ justifyContent: 'center', alignItems: 'center', display:'flex', width: 1, height: 1 }}>
+        <Box sx={{ bgColor: 'primary.main', justifyContent: 'center', alignItems: 'center', display:'flex', width: 1, height: 1 }}>
             <Typography gutterBottom variant={props.variant} component="div">
                 {props.children}
             </Typography>
